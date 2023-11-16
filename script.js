0affc93f33439ed608d42d6b1eab4d0123d4b84e
PRABHAKAR MISHRA <114891680+Prabhakarmishrafrontend@users.noreply.github.com>
2023-11-16 20:55:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySlidebar").style.width = "250px";
+
+    document.getElementById("main").style.marginLeft  = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySlidebar").style.width = "0";
+
+    document.getElementById("main").style.marginLeft  = "0";
+}
